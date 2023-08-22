9a139f6c1b167faaccdf201e713d91f01649b092
kristina-abobutsoae <abobutsoaekristina@gmail.com>
2023-08-22 22:16:22 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,106 +5,88 @@ import fetchImages from './pixaby-api';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
-const endText = document.querySelector('.end-text')
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
 let pageNumber = 1;
 let currentHits = 0;
 let searchQuery = '';
-
-loadMoreBtn.style.display = 'none';
-endText.style.display = 'none';
-
+let isLoading = false;
 
 function renderImageList(images) {
-    console.log(images, 'images');
-    const markup = images
-      .map(image => {
-        console.log('img', image);
-        return `<div class="photo-card">
-         <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
-          <div class="info">
-             <p class="info-item">
-      <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
-  </p>
-              <p class="info-item">
-                  <b>Views</b> <span class="info-item-api">${image.views}</span>
-              </p>
-              <p class="info-item">
-                  <b>Comments</b> <span class="info-item-api">${image.comments}</span>
-              </p>
-              <p class="info-item">
-                  <b>Downloads</b> <span class="info-item-api">${image.downloads}</span>
-              </p>
-          </div>
-      </div>`;
-      })
-      .join('');
-    gallery.innerHTML += markup;
-  }
+  const markup = images
+    .map(image => {
+      return `<div class="photo-card">
+       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+        <div class="info">
+           <p class="info-item">
+    <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+</p>
+            <p class="info-item">
+                <b>Views</b> <span class="info-item-api">${image.views}</span>
+            </p>
+            <p class="info-item">
+                <b>Comments</b> <span class="info-item-api">${image.comments}</span>
+            </p>
+            <p class="info-item">
+                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span>
+            </p>
+        </div>
+    </div>`;
+    })
+    .join('');
+  gallery.innerHTML += markup;
+}
 
 searchForm.addEventListener('submit', onSubmitSearchForm);
 
 async function onSubmitSearchForm(e) {
-    e.preventDefault();
-    searchQuery = e.currentTarget.searchQuery.value.trim();
-    pageNumber = 1;
-  
-    if (!searchQuery) {
-      message('Please write correct data!');
-      return;
-    }
+  e.preventDefault();
+  searchQuery = e.currentTarget.searchQuery.value.trim();
+  pageNumber = 1;
 
-    const response = await fetchImages(searchQuery, pageNumber);
-    currentHits = response.hits.length;
-  
-    if (response.totalHits > 40) {
-      loadMoreBtn.style.display = 'block';
-    } else {
-      loadMoreBtn.style.display = 'none';
-    }
-  
-    try {
-      if (response.total === 0) {
-        message('Please write correct data!');
-        return;
-      }
-      currentHits += response.hits.length;
-      if (currentHits === response.totalHits || currentHits < 40) {
-        refs.endText.textContent =
-          'Were sorry, but you ve reached the end of search results.';
-        return;
-      }
-      if (response.totalHits > 0) {
-        Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
-        gallery.innerHTML = '';
-        renderImageList (response.hits);
-        gallerySimpleLightbox.refresh();
-      }
-  
-      if (response.totalHits === 0) {
-        gallery.innerHTML = '';
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        loadMoreBtn.style.display = 'none';
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  if (!searchQuery) {
+    Notiflix.Notify.failure('Please write correct data!');
+    return;
   }
-  
-  loadMoreBtn.addEventListener('click', onClickLoadMoreBtn);
-  
-  async function onClickLoadMoreBtn() {
-    pageNumber ++;
-    const response = await fetchImages(searchQuery, pageNumber);
+
+  const response = await fetchImages(searchQuery, pageNumber);
+  currentHits = response.hits.length;
+
+  if (response.totalHits === 0) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    return;
+  }
+
+  if (response.totalHits > 0) {
+    Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
+    gallery.innerHTML = '';
     renderImageList(response.hits);
     gallerySimpleLightbox.refresh();
-    currentHits += response.hits.length;
-  
-    if (currentHits === response.totalHits) {
-      loadMoreBtn.style.display = 'none';
-      endText.style.display = 'block';
-    }
   }
-  
+}
+
+window.addEventListener('scroll', () => {
+  const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 100 && !isLoading) {
+    loadMoreImages();
+  }
+});
+
+function loadMoreImages() {
+  if (!isLoading) {
+    isLoading = true;
+    pageNumber += 1;
+    fetchImages(searchQuery, pageNumber)
+      .then(response => {
+        renderImageList(response.hits);
+        gallerySimpleLightbox.refresh();
+        currentHits += response.hits.length;
+        isLoading = false;
+      })
+      .catch(error => {
+        console.error(error);
+        isLoading = false;
+      });
+  }
+}
