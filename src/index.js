cdc5357ba091be8bc1aa8aef4a46566ec3f3beac
kristina-abobutsoae <abobutsoaekristina@gmail.com>
2023-08-23 17:25:55 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import fetchImages from './pixaby-api';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
@@ -11,30 +10,27 @@ let pageNumber = 1;
 let currentHits = 0;
 let searchQuery = '';
 let isLoading = false;
+let isLastPage = false; 
 
 function renderImageList(images) {
-  const markup = images
-    .map(image => {
-      return `<div class="photo-card">
-       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
-        <div class="info">
-           <p class="info-item">
-    <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
-</p>
-            <p class="info-item">
-                <b>Views</b> <span class="info-item-api">${image.views}</span>
-            </p>
-            <p class="info-item">
-                <b>Comments</b> <span class="info-item-api">${image.comments}</span>
-            </p>
-            <p class="info-item">
-                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span>
-            </p>
-        </div>
-    </div>`;
-    })
-    .join('');
-  gallery.innerHTML += markup;
+  const fragment = document.createDocumentFragment();
+  images.forEach((image) => {
+    const photoCard = document.createElement('div');
+    photoCard.classList.add('photo-card');
+    photoCard.innerHTML = `
+      <a href="${image.largeImageURL}">
+        <img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/>
+      </a>
+      <div class="info">
+        <p class="info-item"><b>Likes</b> <span class="info-item-api">${image.likes}</span></p>
+        <p class="info-item"><b>Views</b> <span class="info-item-api">${image.views}</span></p>
+        <p class="info-item"><b>Comments</b> <span class="info-item-api">${image.comments}</span></p>
+        <p class="info-item"><b>Downloads</b> <span class="info-item-api">${image.downloads}</span></p>
+      </div>
+    `;
+    fragment.appendChild(photoCard);
+  });
+  gallery.appendChild(fragment);
 }
 
 searchForm.addEventListener('submit', onSubmitSearchForm);
@@ -43,6 +39,7 @@ async function onSubmitSearchForm(e) {
   e.preventDefault();
   searchQuery = e.currentTarget.searchQuery.value.trim();
   pageNumber = 1;
+  isLastPage = false;
 
   if (!searchQuery) {
     Notiflix.Notify.failure('Please write correct data!');
@@ -65,26 +62,31 @@ async function onSubmitSearchForm(e) {
   }
 }
 
-window.addEventListener('scroll', () => {
-  const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1,
+};
 
-  if (scrollTop + clientHeight >= scrollHeight - 100 && !isLoading) {
-    loadMoreImages();
-  }
-});
+const observer = new IntersectionObserver(loadMoreImages, options);
+observer.observe(document.querySelector('.photo-card:last-child'));
 
-function loadMoreImages() {
-  if (!isLoading) {
+function loadMoreImages(entries) {
+  if (entries[0].isIntersecting && !isLoading && currentHits > 0 && !isLastPage) {
     isLoading = true;
     pageNumber += 1;
     fetchImages(searchQuery, pageNumber)
-      .then(response => {
-        renderImageList(response.hits);
-        gallerySimpleLightbox.refresh();
-        currentHits += response.hits.length;
-        isLoading = false;
+      .then((response) => {
+        if (response.hits.length === 0) {
+          isLastPage = true; 
+        } else {
+          renderImageList(response.hits);
+          gallerySimpleLightbox.refresh();
+          currentHits += response.hits.length;
+          isLoading = false;
+        }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         isLoading = false;
       });
