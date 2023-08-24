e44d20d4835cff4feef0f414747e2448742cc55b
kristina-abobutsoae <abobutsoaekristina@gmail.com>
2023-08-24 15:10:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,27 +11,30 @@ let pageNumber = 1;
 let currentHits = 0;
 let searchQuery = '';
 let isLoading = false;
-let isLastPage = false;
 
 function renderImageList(images) {
-  const fragment = document.createDocumentFragment();
-  images.forEach((image) => {
-    const photoCard = document.createElement('div');
-    photoCard.classList.add('photo-card');
-    photoCard.innerHTML = `
-      <a href="${image.largeImageURL}">
-        <img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/>
-      </a>
-      <div class="info">
-        <p class="info-item"><b>Likes</b> <span class="info-item-api">${image.likes}</span></p>
-        <p class="info-item"><b>Views</b> <span class="info-item-api">${image.views}</span></p>
-        <p class="info-item"><b>Comments</b> <span class="info-item-api">${image.comments}</span></p>
-        <p class="info-item"><b>Downloads</b> <span class="info-item-api">${image.downloads}</span></p>
-      </div>
-    `;
-    fragment.appendChild(photoCard);
-  });
-  gallery.appendChild(fragment);
+  const markup = images
+    .map(image => {
+      return `<div class="photo-card">
+       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+        <div class="info">
+           <p class="info-item">
+    <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+</p>
+            <p class="info-item">
+                <b>Views</b> <span class="info-item-api">${image.views}</span>
+            </p>
+            <p class="info-item">
+                <b>Comments</b> <span class="info-item-api">${image.comments}</span>
+            </p>
+            <p class="info-item">
+                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span>
+            </p>
+        </div>
+    </div>`;
+    })
+    .join('');
+  gallery.innerHTML = markup;
 }
 
 searchForm.addEventListener('submit', onSubmitSearchForm);
@@ -40,7 +43,6 @@ async function onSubmitSearchForm(e) {
   e.preventDefault();
   searchQuery = e.currentTarget.searchQuery.value.trim();
   pageNumber = 1;
-  isLastPage = false;
 
   if (!searchQuery) {
     Notiflix.Notify.failure('Please write correct data!');
@@ -57,37 +59,40 @@ async function onSubmitSearchForm(e) {
 
   if (response.totalHits > 0) {
     Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
-    gallery.innerHTML = '';
     renderImageList(response.hits);
     gallerySimpleLightbox.refresh();
   }
 }
 
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.1,
-};
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && !isLoading) {
+      loadMoreImages();
+    }
+  });
+}, {
+  root: document.body,
+  threshold: 0.9
+});
 
-const observer = new IntersectionObserver(loadMoreImages, options);
-observer.observe(document.querySelector('.photo-card:last-child'));
-
-function loadMoreImages(entries) {
-  if (entries[0].isIntersecting && !isLoading && currentHits > 0 && !isLastPage) {
+function loadMoreImages() {
+  if (!isLoading) {
     isLoading = true;
     pageNumber += 1;
     fetchImages(searchQuery, pageNumber)
-      .then((response) => {
-        if (response.hits.length === 0) {
-          isLastPage = true;
-        } else {
-          renderImageList(response.hits);
-          gallerySimpleLightbox.refresh();
-          currentHits += response.hits.length;
+      .then(response => {
+        if (response.totalHits <= currentHits) {
+          // We've reached the last page
           isLoading = false;
+          return;
         }
+
+        renderImageList(response.hits);
+        gallerySimpleLightbox.refresh();
+        currentHits += response.hits.length;
+        isLoading = false;
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
         isLoading = false;
       });
